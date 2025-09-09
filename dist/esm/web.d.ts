89b86c49e8fbf43213438144e4d0d0cdafc1a31e
massimiliano <maxcoppola97@libero.it>
2025-09-09 15:09:52 +0200

    we need dist folder

---
+++ dist/esm/web.d.ts
@@ -0,0 +1,8 @@
+import { WebPlugin } from "@capacitor/core";
+import type { StartNavigationOptions, StartNavigationPlugin } from "./definitions";
+export declare class StartNavigationPluginWeb extends WebPlugin implements StartNavigationPlugin {
+    launchMapsApp(options: StartNavigationOptions): Promise<void>;
+}
+declare const StartNavigation: StartNavigationPluginWeb;
+export * from "./definitions";
+export { StartNavigation };
